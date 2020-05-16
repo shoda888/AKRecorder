43bad6f9d93d8e59b1bb17ba5381f1a63ecd4547
shoda888 <gekka888@gmail.com>
2020-05-16 14:51:08 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Azure.Kinect.BodyTracking;
+using Microsoft.Azure.Kinect.Sensor;
+using System;
+using System.Data;
+using System.Drawing;
+using Image = Microsoft.Azure.Kinect.Sensor.Image;
+using BitmapData = System.Drawing.Imaging.BitmapData;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
+using System.Threading.Tasks;
+
+namespace Csharp_3d_viewer
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            using (var visualizerData = new VisualizerData())
+            {
+                var renderer = new Renderer(visualizerData);
+
+                renderer.StartVisualizationThread();
+
+                // Open device.
+                using (Device device = Device.Open())
+                {
+                    device.StartCameras(new DeviceConfiguration()
+                    {
+                        ColorFormat = ImageFormat.ColorBGRA32,
+                        ColorResolution = ColorResolution.R720p,
+                        DepthMode = DepthMode.NFOV_Unbinned,
+                        SynchronizedImagesOnly = true,
+                        WiredSyncMode = WiredSyncMode.Standalone,
+                        CameraFPS = FPS.FPS30
+                    });
+
+                    var deviceCalibration = device.GetCalibration();
+                    var transformation = deviceCalibration.CreateTransformation();
+                    PointCloud.ComputePointCloudCache(deviceCalibration);
+
+                    using (Tracker tracker = Tracker.Create(deviceCalibration, new TrackerConfiguration() { ProcessingMode = TrackerProcessingMode.Gpu, SensorOrientation = SensorOrientation.Default }))
+                    {
+                        while (renderer.IsActive)
+                        {
+                            using (Capture sensorCapture = await Task.Run(() => device.GetCapture()).ConfigureAwait(true))
+                            {
+                                // Queue latest frame from the sensor.
+                                tracker.EnqueueCapture(sensorCapture);
+                                if (renderer.IsHuman)
+                                {
+                                    unsafe
+                                    {
+                                        //Depth画像の横幅(width)と縦幅(height)を取得
+                                        int depth_width = device.GetCalibration().DepthCameraCalibration.ResolutionWidth;
+                                        int depth_height = device.GetCalibration().DepthCameraCalibration.ResolutionHeight;
+                                        // Bitmap depthBitmap = new Bitmap(depth_width, depth_height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                                        Bitmap colorBitmap = new Bitmap(depth_width, depth_height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+                                        // Image depthImage = sensorCapture.Depth;
+                                        Image colorImage = transformation.ColorImageToDepthCamera(sensorCapture);
+                                        // ushort[] depthArray = depthImage.GetPixels<ushort>().ToArray();
+                                        BGRA[] colorArray = colorImage.GetPixels<BGRA>().ToArray();
+                                        // BitmapData bitmapData = depthBitmap.LockBits(new Rectangle(0, 0, depthBitmap.Width, depthBitmap.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+                                        BitmapData bitmapData = colorBitmap.LockBits(new Rectangle(0, 0, colorBitmap.Width, colorBitmap.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+                                        //各ピクセルの値へのポインタ
+                                        byte* pixels = (byte*)bitmapData.Scan0;
+                                        int index = 0;
+                                        //一ピクセルずつ処理
+                                        for (int i = 0; i < colorArray.Length; i++)
+                                        {
+                                            pixels[index++] = colorArray[i].B;
+                                            pixels[index++] = colorArray[i].G;
+                                            pixels[index++] = colorArray[i].R;
+                                            pixels[index++] = 255;//Alpha値を固定して不透過に
+                                        }
+                                        //書き込み終了
+                                        colorBitmap.UnlockBits(bitmapData);
+                                        string time = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                                        // DirectoryUtils.SafeCreateDirectory($@"C:\Users\gekka\temp\{this.scene}");
+                                        colorBitmap.Save($@"C:\Users\gekka\temp\{renderer.scene}\depth\{time}.png", System.Drawing.Imaging.ImageFormat.Png);
+                                    }
+                                }
+                            }
+
+                            // Try getting latest tracker frame.
+                            using (Frame frame = tracker.PopResult(TimeSpan.Zero, throwOnTimeout: false))
+                            {
+                                if (frame != null)
+                                {
+                                    // Save this frame for visualization in Renderer.
+
+                                    // One can access frame data here and extract e.g. tracked bodies from it for the needed purpose.
+                                    // Instead, for simplicity, we transfer the frame object to the rendering background thread.
+                                    // This example shows that frame popped from tracker should be disposed. Since here it is used
+                                    // in a different thread, we use Reference method to prolong the lifetime of the frame object.
+                                    // For reference on how to read frame data, please take a look at Renderer.NativeWindow_Render().
+                                    visualizerData.Frame = frame.Reference();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Renderer.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.Azure.Kinect.BodyTracking;
+using OpenGL;
+using OpenGL.CoreUI;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace Csharp_3d_viewer
+{
+    public class Renderer
+    {
+        private SphereRenderer SphereRenderer;
+        private CylinderRenderer CylinderRenderer;
+        private PointCloudRenderer PointCloudRenderer;
+
+        private readonly VisualizerData visualizerData;
+        private List<Vertex> pointCloud = null;
+        public Renderer(VisualizerData visualizerData)
+        {
+            this.visualizerData = visualizerData;
+        }
+
+        public bool IsActive { get; private set; }
+
+        public bool IsHuman { get; private set; } = false;
+
+        public string scene;
+
+        public void StartVisualizationThread()
+        {
+            Task.Run(() =>
+            {
+                using (NativeWindow nativeWindow = NativeWindow.Create())
+                {
+                    IsActive = true;
+                    nativeWindow.ContextCreated += NativeWindow_ContextCreated;
+                    nativeWindow.Render += NativeWindow_Render;
+                    nativeWindow.KeyDown += (object obj, NativeWindowKeyEventArgs e) =>
+                    {
+                        switch (e.Key)
+                        {
+                            case KeyCode.Escape:
+                                nativeWindow.Stop();
+                                IsActive = false;
+                                break;
+
+                            case KeyCode.F:
+                                nativeWindow.Fullscreen = !nativeWindow.Fullscreen;
+                                break;
+                        }
+                    };
+                    nativeWindow.Animation = true;
+
+                    nativeWindow.Create(0, 0, 640, 480, NativeWindowStyle.Overlapped);
+
+                    nativeWindow.Show();
+                    nativeWindow.Run();
+                }
+            });
+        }
+
+        private void NativeWindow_ContextCreated(object sender, NativeWindowEventArgs e)
+        {
+            Gl.ReadBuffer(ReadBufferMode.Back);
+
+            Gl.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+            Gl.Enable(EnableCap.Blend);
+            Gl.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+
+            Gl.LineWidth(2.5f);
+
+            CreateResources();
+        }
+
+        private static float ToRadians(float degrees)
+        {
+            return degrees / 180.0f * (float)Math.PI;
+        }
+
+        public void NativeWindow_Render(object sender, NativeWindowEventArgs e)
+        {
+            using (var lastFrame = visualizerData.TakeFrameWithOwnership())
+            {
+                if (lastFrame == null)
+                {
+                    return;
+                }
+
+                NativeWindow nativeWindow = (NativeWindow)sender;
+
+                Gl.Viewport(0, 0, (int)nativeWindow.Width, (int)nativeWindow.Height);
+                Gl.Clear(ClearBufferMask.ColorBufferBit);
+
+                // Update model/view/projective matrices in shader
+                var proj = Matrix4x4.CreatePerspectiveFieldOfView(ToRadians(65.0f), (float)nativeWindow.Width / nativeWindow.Height, 0.1f, 150.0f);
+                var view = Matrix4x4.CreateLookAt(Vector3.Zero, Vector3.UnitZ, -Vector3.UnitY);
+
+                SphereRenderer.View = view;
+                SphereRenderer.Projection = proj;
+
+                CylinderRenderer.View = view;
+                CylinderRenderer.Projection = proj;
+
+                PointCloudRenderer.View = view;
+                PointCloudRenderer.Projection = proj;
+
+                PointCloud.ComputePointCloud(lastFrame.Capture.Depth, ref pointCloud);
+                PointCloudRenderer.Render(pointCloud, new Vector4(1, 1, 1, 1));
+
+                if(lastFrame.NumberOfBodies > 0)
+                {
+                    if (!IsHuman)
+                    {
+                        this.scene = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                        string path = $@"C:\Users\gekka\temp\{this.scene}\depth";
+                        Directory.CreateDirectory(path);
+                        IsHuman = true;
+                    }
+                    for (uint i = 0; i < lastFrame.NumberOfBodies; ++i)
+                    {
+                        // System.Diagnostics.Debug.WriteLine(i);
+                        DirectoryUtils.SafeCreateDirectory($@"C:\Users\gekka\temp\{this.scene}\{i}");
+                        string filename = $@"C:\Users\gekka\temp\{this.scene}\{i}\pos.csv";
+                        var append = true;
+                        var skeleton = lastFrame.GetBodySkeleton(i);
+                        var bodyId = lastFrame.GetBodyId(i);
+                        var bodyColor = BodyColors.GetColorAsVector(bodyId);
+
+                        using (var sw = new System.IO.StreamWriter(filename, append))
+                        {
+                            sw.Write("{0}, ", DateTime.Now.ToString("yyyyMMddHHmmssfff"));
+                            for (int jointId = 0; jointId < (int)JointId.Count; ++jointId)
+                            {
+                                var joint = skeleton.GetJoint(jointId);
+                                sw.Write("{0}, {1}, {2},", joint.Position.X, joint.Position.Y, joint.Position.Z);
+                                
+                                //GUI描画する場合
+                                // Render the joint as a sphere.
+                                const float radius = 0.024f;
+                                SphereRenderer.Render(joint.Position / 1000, radius, bodyColor);
+
+                                if (JointConnections.JointParent.TryGetValue((JointId)jointId, out JointId parentId))
+                                {
+                                    // Render a bone connecting this joint and its parent as a cylinder.
+                                    CylinderRenderer.Render(joint.Position / 1000, skeleton.GetJoint((int)parentId).Position / 1000, bodyColor);
+                                }
+                            }
+                            sw.Write("\r\n"); 
+                        }
+
+                    }
+                }
+                else
+                {
+                    IsHuman = false;
+                }
+
+            }
+        }
+
+        private void CreateResources()
+        {
+            SphereRenderer = new SphereRenderer();
+            CylinderRenderer = new CylinderRenderer();
+            PointCloudRenderer = new PointCloudRenderer();
+        }
+        public static class DirectoryUtils
+        {
+            /// <summary>
+            /// 指定したパスにディレクトリが存在しない場合
+            /// すべてのディレクトリとサブディレクトリを作成します
+            /// </summary>
+            public static DirectoryInfo SafeCreateDirectory( string path )
+            {
+                if ( Directory.Exists( path ) )
+                {
+                    return null;
+                }
+                return Directory.CreateDirectory( path );
+            }
+        }
+    }
+}
